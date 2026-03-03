037abbbbac65f7c2cae73c84d308e5961447260b
Nasma Hmem <hmemnasma@gmail.com>
2026-03-03 04:16:01 +0100

    fix-bug-in-insurance-policy

--- src/main/java/org/example/forsapidev/entities/InsuranceManagement/InsurancePolicy.java
+++ src/main/java/org/example/forsapidev/entities/InsuranceManagement/InsurancePolicy.java
@@ -3,12 +3,16 @@ package org.example.forsapidev.entities.InsuranceManagement;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.example.forsapidev.entities.UserManagement.User;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "insurance_policy")
 public class InsurancePolicy {
 
@@ -88,176 +92,5 @@ public class InsurancePolicy {
         return insuranceProduct != null ? insuranceProduct.getPolicyType() : null;
     }
 
-    // GETTERS AND SETTERS
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
 
-    public String getPolicyNumber() { return policyNumber; }
-    public void setPolicyNumber(String policyNumber) { this.policyNumber = policyNumber; }
-
-    public BigDecimal getPremiumAmount() { return premiumAmount; }
-    public void setPremiumAmount(BigDecimal premiumAmount) { this.premiumAmount = premiumAmount; }
-
-    public BigDecimal getCoverageLimit() { return coverageLimit; }
-    public void setCoverageLimit(BigDecimal coverageLimit) { this.coverageLimit = coverageLimit; }
-
-    public Date getStartDate() { return startDate; }
-    public void setStartDate(Date startDate) { this.startDate = startDate; }
-
-    public Date getEndDate() { return endDate; }
-    public void setEndDate(Date endDate) { this.endDate = endDate; }
-
-    public Date getNextPremiumDueDate() { return nextPremiumDueDate; }
-    public void setNextPremiumDueDate(Date nextPremiumDueDate) { this.nextPremiumDueDate = nextPremiumDueDate; }
-
-    public PolicyStatus getStatus() { return status; }
-    public void setStatus(PolicyStatus status) { this.status = status; }
-
-    // Actuarial fields
-    public BigDecimal getPurePremium() { return purePremium; }
-    public void setPurePremium(BigDecimal purePremium) { this.purePremium = purePremium; }
-
-    public BigDecimal getInventoryPremium() { return inventoryPremium; }
-    public void setInventoryPremium(BigDecimal inventoryPremium) { this.inventoryPremium = inventoryPremium; }
-
-    public BigDecimal getCommercialPremium() { return commercialPremium; }
-    public void setCommercialPremium(BigDecimal commercialPremium) { this.commercialPremium = commercialPremium; }
-
-    public BigDecimal getFinalPremium() { return finalPremium; }
-    public void setFinalPremium(BigDecimal finalPremium) { this.finalPremium = finalPremium; }
-
-    public Double getRiskScore() { return riskScore; }
-    public void setRiskScore(Double riskScore) { this.riskScore = riskScore; }
-
-    public String getRiskCategory() { return riskCategory; }
-    public void setRiskCategory(String riskCategory) { this.riskCategory = riskCategory; }
-
-    public Double getRiskCoefficient() { return riskCoefficient; }
-    public void setRiskCoefficient(Double riskCoefficient) { this.riskCoefficient = riskCoefficient; }
-
-    public String getPaymentFrequency() { return paymentFrequency; }
-    public void setPaymentFrequency(String paymentFrequency) { this.paymentFrequency = paymentFrequency; }
-
-    public BigDecimal getPeriodicPaymentAmount() { return periodicPaymentAmount; }
-    public void setPeriodicPaymentAmount(BigDecimal periodicPaymentAmount) { this.periodicPaymentAmount = periodicPaymentAmount; }
-
-    public Integer getNumberOfPayments() { return numberOfPayments; }
-    public void setNumberOfPayments(Integer numberOfPayments) { this.numberOfPayments = numberOfPayments; }
-
-    public Double getEffectiveAnnualRate() { return effectiveAnnualRate; }
-    public void setEffectiveAnnualRate(Double effectiveAnnualRate) { this.effectiveAnnualRate = effectiveAnnualRate; }
-
-    public String getCalculationNotes() { return calculationNotes; }
-    public void setCalculationNotes(String calculationNotes) { this.calculationNotes = calculationNotes; }
-
-    // Relationships
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
-
-    public InsuranceProduct getInsuranceProduct() { return insuranceProduct; }
-    public void setInsuranceProduct(InsuranceProduct insuranceProduct) { this.insuranceProduct = insuranceProduct; }
-
-    public Set<PremiumPayment> getPremiumPayments() { return premiumPayments; }
-    public void setPremiumPayments(Set<PremiumPayment> premiumPayments) { this.premiumPayments = premiumPayments; }
-
-    public Set<InsuranceClaim> getClaims() { return claims; }
-    public void setClaims(Set<InsuranceClaim> claims) { this.claims = claims; }
-
-    // Getters and Setters for actuarial fields
-
-    public String getCalculationNotes() {
-        return calculationNotes;
-    }
-
-    public void setCalculationNotes(String calculationNotes) {
-        this.calculationNotes = calculationNotes;
-    }
-
-    public Double getEffectiveAnnualRate() {
-        return effectiveAnnualRate;
-    }
-
-    public void setEffectiveAnnualRate(Double effectiveAnnualRate) {
-        this.effectiveAnnualRate = effectiveAnnualRate;
-    }
-
-    public Integer getNumberOfPayments() {
-        return numberOfPayments;
-    }
-
-    public void setNumberOfPayments(Integer numberOfPayments) {
-        this.numberOfPayments = numberOfPayments;
-    }
-
-    public BigDecimal getPeriodicPaymentAmount() {
-        return periodicPaymentAmount;
-    }
-
-    public void setPeriodicPaymentAmount(BigDecimal periodicPaymentAmount) {
-        this.periodicPaymentAmount = periodicPaymentAmount;
-    }
-
-    public String getPaymentFrequency() {
-        return paymentFrequency;
-    }
-
-    public void setPaymentFrequency(String paymentFrequency) {
-        this.paymentFrequency = paymentFrequency;
-    }
-
-    public Double getRiskCoefficient() {
-        return riskCoefficient;
-    }
-
-    public void setRiskCoefficient(Double riskCoefficient) {
-        this.riskCoefficient = riskCoefficient;
-    }
-
-    public String getRiskCategory() {
-        return riskCategory;
-    }
-
-    public void setRiskCategory(String riskCategory) {
-        this.riskCategory = riskCategory;
-    }
-
-    public Double getRiskScore() {
-        return riskScore;
-    }
-
-    public void setRiskScore(Double riskScore) {
-        this.riskScore = riskScore;
-    }
-
-    public BigDecimal getFinalPremium() {
-        return finalPremium;
-    }
-
-    public void setFinalPremium(BigDecimal finalPremium) {
-        this.finalPremium = finalPremium;
-    }
-
-    public BigDecimal getCommercialPremium() {
-        return commercialPremium;
-    }
-
-    public void setCommercialPremium(BigDecimal commercialPremium) {
-        this.commercialPremium = commercialPremium;
-    }
-
-    public BigDecimal getInventoryPremium() {
-        return inventoryPremium;
-    }
-
-    public void setInventoryPremium(BigDecimal inventoryPremium) {
-        this.inventoryPremium = inventoryPremium;
-    }
-
-    public BigDecimal getPurePremium() {
-        return purePremium;
-    }
-
-    public void setPurePremium(BigDecimal purePremium) {
-        this.purePremium = purePremium;
-    }
 }
